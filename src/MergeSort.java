9f008eb53d3297330e5da209468b1e0af70cd4d2
User <dg21stew@gmail.com>
2023-01-20 15:33:14 -0800

    spacing

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,33 +1,42 @@
 import java.util.Arrays;
 
 public class MergeSort {
+
     public static void main(String[] args) {
+
         int[] arr = {19 ,65 ,23, 3, 1, 4, 2, 5, 9, 15};
         mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void mergeSort(int[] arr) {
+
         // base case, if array is less than 2, it's already sorted
         if (arr.length < 2) {
             return;
         }
         // find the middle index of the array
         int middle = arr.length / 2;
+
         // split the array in half
         int[] left = Arrays.copyOfRange(arr, 0, middle);
         int[] right = Arrays.copyOfRange(arr, middle, arr.length);
+
         // recursively sort the left and right arrays
         mergeSort(left);
         mergeSort(right);
+
         // merge the sorted left and right arrays back into the original array
         merge(arr, left, right);
     }
 
     public static void merge(int[] arr, int[] left, int[] right) {
+
         int i = 0, j = 0, k = 0;
+
         // while there are still elements in both arrays
         while (i < left.length && j < right.length) {
+
             // add the smaller element to the final array
             if (left[i] < right[j]) {
                 arr[k++] = left[i++];
